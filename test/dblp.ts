2ee2909fe4e19dcbb5363161f1cd4331fdc415fb
Andre Restivo <andre.restivo@gmail.com>
2018-11-15 20:52:24 +0000

    Fixed dblp tests

--- test/dblp.ts
+++ test/dblp.ts
@@ -2,9 +2,32 @@ import { expect } from 'chai'
 import { DBLPScraper } from '../src/scraper/DBLPScraper'
 
 describe('DBLPScraper', () => {
-  it('should return articles', () => {
+
+  it('should return articles', (done) => {
     let dblp = new DBLPScraper()
-    let articles = dblp.query('blockchain')
-    //expect(articles.length).to.be.greaterThan(0);
+    dblp.query('blockchain')
+    .then(function(articles) {
+      try {
+        expect(articles.length).to.be.greaterThan(0)
+        done()  
+      } catch(err) {
+        done(err)
+      }
+    }, done)
+  }),
+
+  it('should contain relevant articles only', (done) => {
+    let dblp = new DBLPScraper()
+    dblp.query('blockchain')
+    .then(function(articles) {
+      try {
+        let relevant = articles.filter((article : any) => article.title.toLowerCase().includes('blockchain'))
+        expect(articles.length).to.be.equal(relevant.length)
+        done()  
+      } catch(err) {
+        done(err)
+      }
+    }, done)
   })
+
 })
