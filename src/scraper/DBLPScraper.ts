895d10f65a001f85f84a61b79b442646994c0bad
Andre Restivo <andre.restivo@gmail.com>
2018-11-15 21:45:59 +0000

    Fixed await/async as Hugo was bugging me

--- src/scraper/DBLPScraper.ts
+++ src/scraper/DBLPScraper.ts
@@ -5,12 +5,17 @@ class DBLPScraper extends Scraper {
   uri : string = 'http://dblp.org/search/publ/api'
 
   async query(q : string): Promise<any> {
-    return this.get(this.uri, {q : q, format : 'json'})
-    .then(function (response) {
-      return response.data.result.hits.hit.map(
-        (hit : any) => ({title: hit.info.title, year: hit.info.year})
-      )
-    })
+    let articles = await this.get(this.uri, {q : q, format : 'json'})
+    return articles.data.result.hits.hit.map(
+      (hit : any) => ({
+        title: hit.info.title, 
+        year: hit.info.year,
+        doi: hit.info.doi,
+        authors: Array.isArray(hit.info.authors.author) ? 
+          hit.info.authors.author.join(', ') : 
+          hit.info.authors.author
+      })
+    )
   }
 }
 
