f8c1a1b55fb80db64f76f63f723462dc409955b4
Andre Restivo <andre.restivo@gmail.com>
2018-11-16 10:22:25 +0000

    More let to const

--- test/dblp.ts
+++ test/dblp.ts
@@ -4,7 +4,7 @@ import { DBLPScraper } from '../src/scraper/DBLPScraper'
 describe('DBLPScraper', () => {
 
   it('should return articles', (done) => {
-    let dblp = new DBLPScraper()
+    const dblp = new DBLPScraper()
     dblp.query('blockchain')
     .then(function(articles) {
       try {
@@ -17,11 +17,11 @@ describe('DBLPScraper', () => {
   }),
 
   it('should contain relevant articles only', (done) => {
-    let dblp = new DBLPScraper()
+    const dblp = new DBLPScraper()
     dblp.query('blockchain')
     .then(function(articles) {
       try {
-        let relevant = articles.filter((article : any) => article.title.toLowerCase().includes('blockchain'))
+        const relevant = articles.filter((article : any) => article.title.toLowerCase().includes('blockchain'))
         expect(articles.length).to.be.equal(relevant.length)
         done()  
       } catch(err) {
@@ -31,7 +31,7 @@ describe('DBLPScraper', () => {
   })
 
   it('should contain only the right amount of articles', (done) => {
-    let dblp = new DBLPScraper()
+    const dblp = new DBLPScraper()
     dblp.query('blockchain', 5)
     .then(function(articles) {
       try {
