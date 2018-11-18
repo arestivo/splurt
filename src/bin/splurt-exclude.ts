2a00a70234ead38c2639a489d2686ecb67005cbc
Hugo Ferreira <bytter@gmail.com>
2018-11-18 17:39:20 +0000

    Linting

--- src/bin/splurt-exclude.ts
+++ src/bin/splurt-exclude.ts
@@ -6,8 +6,8 @@ import program from 'commander'
 import Color from 'colors'
 import YAML from 'yamljs'
 
-function list(l : string) : string[] {
-  return l.split(',').map(v => v.trim());
+function list(l: string): string[] {
+  return l.split(',').map(v => v.trim())
 }
 
 program
@@ -17,9 +17,9 @@ program
   .option('-e, --exclude <criteria>', 'Comma separated exclusion criteria using SQL.', list)
   .option('-s, --sqlite <database>', 'SQLite database used to store articles.')
 
-  .parse(process.argv);
+  .parse(process.argv)
 
-const splurt = new SplurtExclude
+const splurt = new SplurtExclude()
 
 if (program.project) {
   try {
@@ -31,15 +31,13 @@ if (program.project) {
     console.error(Color.red(e.message))
     process.exit()
   }
-} 
+}
 
-splurt.criteria = program.exclude ? program.exclude : splurt.criteria  
-splurt.sqlite = program.sqlite ? program.sqlite : splurt.sqlite 
+splurt.criteria = program.exclude ? program.exclude : splurt.criteria
+splurt.sqlite = program.sqlite ? program.sqlite : splurt.sqlite
 
-try {  
-  splurt.execute().catch(function(e : any){
-    console.error(Color.red(e.message))
-  })
-} catch(e) {
+try {
+  splurt.execute().catch(e => console.error(Color.red(e.message)))
+} catch (e) {
   console.error(Color.red(e.message))
 }
