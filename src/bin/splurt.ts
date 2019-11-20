42b231ceaa90f7913535d2a157c8eb7beb2aaf0a
msramalho <up201403027@fe.up.pt>
2019-11-20 16:31:35 +0000

    updated version number
    in the future, splurt.ts should read it directly from package.json

--- src/bin/splurt.ts
+++ src/bin/splurt.ts
@@ -3,7 +3,7 @@
 import program from 'commander'
 
 program
-  .version('0.0.13')
+  .version('0.0.20')
 
   .command('fetch', 'Fetch article data from selected research databases.').alias('f')
   .command('exclude', 'Exclude articles according to some criteria.').alias('e')
